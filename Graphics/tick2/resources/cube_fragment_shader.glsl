6d6c7e51b42a45b6cf47da057052907131d403f3
Gábor Fuisz <fuiszgabor@gmail.com>
2020-11-27 18:34:57 +0000

    Add template code

---
+++ Graphics/tick2/resources/cube_fragment_shader.glsl
@@ -0,0 +1,34 @@
+#version 140
+
+in vec3 wc_frag_normal;        	// fragment normal in world coordinates (wc_)
+in vec2 frag_texcoord;			// texture UV coordinates
+in vec3 wc_frag_pos;			// fragment position in world coordinates
+
+out vec3 color;			        // pixel colour
+
+uniform sampler2D tex;  		  // 2D texture sampler
+uniform samplerCube skybox;		  // Cubemap texture used for reflections
+uniform vec3 wc_camera_position;  // Position of the camera in world coordinates
+
+// Tone mapping and display encoding combined
+vec3 tonemap(vec3 linearRGB)
+{
+    float L_white = 0.7; // Controls the brightness of the image
+
+    float inverseGamma = 1./2.2;
+    return pow(linearRGB/L_white, vec3(inverseGamma)); // Display encoding - a gamma
+}
+
+
+
+void main()
+{
+	vec3 linear_color = vec3(0, 0, 0);
+	// TODO: Calculate colour using Phong illumination model
+	// linear_color = ambient + diffuse + specular
+	// TODO: Sample the texture and replace diffuse surface colour (C_diff) with texel value
+
+
+	color = tonemap(linear_color);
+}
+
---
+++ Graphics/tick2/src/uk/ac/cam/cl/gfxintro/crsid/tick2/CubeRobot.java
@@ -0,0 +1,164 @@
+package uk.ac.cam.cl.gfxintro.crsid.tick2;
+
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+
+import static org.lwjgl.opengl.GL11.GL_NO_ERROR;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_INT;
+import static org.lwjgl.opengl.GL11.glDrawElements;
+import static org.lwjgl.opengl.GL11.glGetError;
+import static org.lwjgl.opengl.GL20.GL_FRAGMENT_SHADER;
+import static org.lwjgl.opengl.GL20.GL_VERTEX_SHADER;
+import static org.lwjgl.opengl.GL20.glGetUniformLocation;
+import static org.lwjgl.opengl.GL20.glUniformMatrix4fv;
+import static org.lwjgl.opengl.GL20.glUniform3f;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+
+import java.lang.Math;
+import java.nio.FloatBuffer;;
+
+public class CubeRobot {
+	
+    // Filenames for vertex and fragment shader source code
+    private final static String VSHADER_FN = "resources/cube_vertex_shader.glsl";
+    private final static String FSHADER_FN = "resources/cube_fragment_shader.glsl";
+    
+    // Reference to skybox of the scene
+    public SkyBox skybox;
+    
+    // Components of this CubeRobot
+    
+    // Component 1 : Body
+	private Mesh body_mesh;				// Mesh of the body
+	private ShaderProgram body_shader;	// Shader to colour the body mesh
+	private Texture body_texture;		// Texture image to be used by the body shader
+	private Matrix4f body_transform;	// Transformation matrix of the body object
+	
+	// TODO: Add Component 2: Right Arm
+
+	
+	// Complete rest of the robot
+
+	
+/**
+ *  Constructor
+ *  Initialize all the CubeRobot components
+ */
+	public CubeRobot() {
+		// Create body node
+		
+		// Initialise Geometry
+		body_mesh = new CubeMesh(); 
+		
+		// Initialise Shader
+		body_shader = new ShaderProgram(new Shader(GL_VERTEX_SHADER, VSHADER_FN), new Shader(GL_FRAGMENT_SHADER, FSHADER_FN), "colour");
+		// Tell vertex shader where it can find vertex positions. 3 is the dimensionality of vertex position
+		// The prefix "oc_" means object coordinates
+		body_shader.bindDataToShader("oc_position", body_mesh.vertex_handle, 3);
+		// Tell vertex shader where it can find vertex normals. 3 is the dimensionality of vertex normals
+		body_shader.bindDataToShader("oc_normal", body_mesh.normal_handle, 3);
+		// Tell vertex shader where it can find texture coordinates. 2 is the dimensionality of texture coordinates
+		body_shader.bindDataToShader("texcoord", body_mesh.tex_handle, 2);
+		
+		// Initialise Texturing
+		body_texture = new Texture(); 
+		body_texture.load("resources/cubemap.png");
+		
+		// Build Transformation Matrix
+		body_transform = new Matrix4f(); 
+		
+		// TODO: Scale the body transformation matrix
+
+		
+		// TODO: Create right arm node
+
+		
+		// TODO: Initialise Texturing
+
+		
+		// TODO: Build Right Arm's Transformation Matrix (rotate the right arm around its end)
+
+		
+		// TODO: Complete robot
+
+	}
+	
+
+	/**
+	 * Updates the scene and then renders the CubeRobot
+	 * @param camera - Camera to be used for rendering
+	 * @param deltaTime		- Time taken to render this frame in seconds (= 0 when the application is paused)
+	 * @param elapsedTime	- Time elapsed since the beginning of this program in millisecs
+	 */
+	public void render(Camera camera, float deltaTime, long elapsedTime) {
+		
+		// TODO: Animate Body. Translate the body as a function of time
+
+		
+		// TODO: Animate Arm. Rotate the left arm around its end as a function of time
+
+		
+		renderMesh(camera, body_mesh, body_transform, body_shader, body_texture);
+		
+		// TODO: Chain transformation matrices of the arm and body (Scene Graph)
+		// TODO: Render Arm.
+
+		
+		//TODO: Render rest of the robot
+
+		
+		
+
+		
+	}
+	
+	/**
+	 * Draw mesh from a camera perspective
+	 * @param camera		- Camera to be used for rendering
+	 * @param mesh			- mesh to render
+	 * @param modelMatrix	- model transformation matrix of this mesh
+	 * @param shader		- shader to colour this mesh
+	 * @param texture		- texture image to be used by the shader
+	 */
+	public void renderMesh(Camera camera, Mesh mesh , Matrix4f modelMatrix, ShaderProgram shader, Texture texture) {
+		// If shaders modified on disk, reload them
+		shader.reloadIfNeeded(); 
+		shader.useProgram();
+
+		// Step 2: Pass relevant data to the vertex shader
+		
+		// compute and upload MVP
+		Matrix4f mvp_matrix = new Matrix4f(camera.getProjectionMatrix()).mul(camera.getViewMatrix()).mul(modelMatrix);
+		shader.uploadMatrix4f(mvp_matrix, "mvp_matrix");
+		
+		// Upload Model Matrix and Camera Location to the shader for Phong Illumination
+		shader.uploadMatrix4f(modelMatrix, "m_matrix");
+		shader.uploadVector3f(camera.getCameraPosition(), "wc_camera_position");
+		
+		// Transformation by a nonorthogonal matrix does not preserve angles
+		// Thus we need a separate transformation matrix for normals
+		Matrix3f normal_matrix = new Matrix3f();
+		//TODO: Calculate normal transformation matrix
+		
+		shader.uploadMatrix3f(normal_matrix, "normal_matrix");
+		
+		// Step 3: Draw our VertexArray as triangles
+		// Bind Textures
+		texture.bindTexture();
+		shader.bindTextureToShader("tex", 0);
+		skybox.bindCubemap();
+		shader.bindTextureToShader("skybox", 1);
+		// draw
+		glBindVertexArray(mesh.vertexArrayObj); // Bind the existing VertexArray object
+		glDrawElements(GL_TRIANGLES, mesh.no_of_triangles, GL_UNSIGNED_INT, 0); // Draw it as triangles
+		glBindVertexArray(0);             // Remove the binding
+		
+        // Unbind texture
+		texture.unBindTexture();
+		skybox.unBindCubemap();
+	}
+}
